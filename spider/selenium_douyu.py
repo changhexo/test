3a2cf98bf3800f19a3473421f4628ffab57836d5
changhexo <471865477@qq.com>
2018-12-05 17:35:31 +0800

    selenium douyu spider

--- spider/selenium_douyu.py
+++ spider/selenium_douyu.py
@@ -6,7 +6,6 @@ import time
 # Chrome无头模式（无界面模式）
 chrome_options = Options()
 chrome_options.add_argument('--headless')
-
 # 实例化浏览器
 dirvie = webdriver.Chrome(chrome_options=chrome_options)
 dirvie.get('https://www.douyu.com/directory/all')
@@ -26,16 +25,13 @@ dirvie.get('https://www.douyu.com/directory/all')
 # hot_list = dirvie.find_elements_by_class_name('dy-num')
 # for hot in hot_list:
 #     print(hot.text)
-room = []
-
+# 获取页数
 pages = dirvie.find_elements_by_xpath("//div[@id='J-pager']/a")
-print(pages)
 num = int(pages[-3].text)
-print(num)
-print(type(num))
 
 
 def get_content():
+    room = []
     room_list_selenium = dirvie.find_elements_by_xpath("//ul[@id='live-list-contentbox']/li")
     for room_lsit in room_list_selenium:
         # room_name = room_lsit.find_element_by_xpath(".//h3[@class='ellipsis']").text
@@ -49,23 +45,21 @@ def get_content():
         hot = room_lsit.find_element_by_xpath(".//span[@class='dy-num fr']").text
         # # print(hot)
         url = room_lsit.find_element_by_xpath("./a").get_attribute("href")
-
+        # 构建字典
         temp = {'title': title, 'tag': tag, 'name': name, 'hot': hot, 'url': url}
-
         room.append(temp)
-        return room
-
-
-get_content()
+    return room
 
 
 for i in range(num-1):
+    ret = get_content()
+    with open('douyu.txt', 'a', encoding='utf-8') as f:
+        for val in ret:
+            f.write(str(val))
+            f.write('\n')
+    print('第%d页保存成功' % i)
     next_url = dirvie.find_element_by_link_text("下一页").click()
+    print('下一页，等待5秒加载网页(%d)' % (i+1))
     time.sleep(5)
-    get_content()
-
-# print(room_list_selenium)
-for j in room:
-    print(j)
 
 dirvie.quit()
